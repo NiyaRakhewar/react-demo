893602e056d078d382ef26035e21b96600c4730a
Niharika Rakhewar <111850896+NiyaRakhewar@users.noreply.github.com>
2024-03-17 22:00:14 +0530

    add supabase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { supabase } from "./supabaseClient";
+export default function App() {
+  const login = async () => {
+    await supabase.auth.signIn({ provider: "github" });
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={login}>Login with github</button>
     </div>
   );
 }
-
-export default App;
